f0e1bb2535bbcbec6adbeb17ae3086b3c04dae14
judge_happily <you3192001@gmail.com>
2024-01-24 12:59:16 +0900

    Initial Commit

---
+++ GPSPrivacy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GPSPrivacy
+//
+//  Created by Jae hyung Kim on 1/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
